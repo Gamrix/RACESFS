c20a885e8e87c125ce6309ea68fbd12715259196
root <root@lisp.cs.ucsb.edu>
2015-11-08 01:46:19 -0800

    refacotored git scritpt so that git works correctly

--- scripts/bootstrap_new_git.sh
+++ scripts/bootstrap_new_git.sh
@@ -4,13 +4,23 @@
 sudo yum install -y curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel
 sudo yum install -y epel-release
 sudo yum install -y asciidoc xmlto docbook2X
+
+# yeah, we will also need gcc...
+sudo yum install -y gcc
+
 sudo ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
 
+mkdir git-src
+cd git-src
 wget https://github.com/git/git/archive/v2.4.10.tar.gz
 
-tar -zxf git-2.4.10.tar.gz
+tar -zxf v2.4.10.tar.gz
 cd git-2.4.10
 make configure
 ./configure --prefix=/usr
 make all doc info
 sudo make install install-doc install-html install-info
+cd ../..
+rm -rf git-src
+
+
